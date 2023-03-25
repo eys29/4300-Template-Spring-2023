0c66eee4837a1d8ce32a10521b36daca93128278
eys29 <eys29@cornell.edu>
2023-03-25 16:06:34 -0400

    location edit distance

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+import numpy as np
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -41,5 +42,40 @@ def home():
 def episodes_search():
     text = request.args.get("title")
     return sql_search(text)
+
+def edit_distance(query, message):
+    query = query.lower()
+    message = message.lower()
+    m = len(query) + 1
+    n = len(message) + 1
+    delete = 1
+    insert = 1
+    substitute = 1
+
+    edit_matrix = np.zeros((m,n))
+    for i in range(1, m): 
+        edit_matrix[i][0] = edit_matrix[i-1][0] + delete
+    for j in range(1, n): 
+        edit_matrix[0][j] = edit_matrix[0][j-1] + insert
+    for i in range(1, m):
+        for j in range(1, n):
+            edit_matrix[i][j] = min(
+                edit_matrix[i-1][j] + delete,
+                edit_matrix[i][j-1] + insert,
+                0 if query == message else edit_matrix[i-1][j-1] + substitute
+            )
+    return edit_matrix[m-1][n-1]
+
+def location_search():
+    location_query = request.args.get("location")
+    query_sql = "SELECT * FROM locations"
+    data = mysql_engine.query_selector(query_sql)
+    results = []
+    for l in data:
+        edit_distance = edit_distance(location_query, l)
+        results.append((edit_distance, l))
+    results.sort(reverse=True)
+    return (i[1] for i in results[:10])
+
 
 # app.run(debug=True)
