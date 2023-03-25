c78008722852bb0cff203da36ca7aa64ada3e8db
Vivian Nguyen <viviamguyen@gmail.com>
2023-03-25 16:46:06 -0400

    Add initial tfidf similarity

---
+++ backend/menu_similarity.py
@@ -0,0 +1,19 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+docs = []
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(docs)
+
+
+def compute_query_cosine_similarities(query):
+    query_vector = vectorizer.transform([query])
+    sims = cosine_similarity(query_vector, tfidf_matrix).flatten()
+    return sims
+
+
+def get_k_recommendations(query, k):
+    sims = compute_query_cosine_similarities(query)
+    indices = np.argsort(sims)[::-1][:k]
+    return docs[indices]
