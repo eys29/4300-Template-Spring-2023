502668976216dc297d1209acd69becef1534c897
Prithwish Dan <dan.prithwish@gmail.com>
2023-03-28 13:28:45 -0400

    Updated database

--- backend/extra_processing.py
+++ backend/extra_processing.py
@@ -4,15 +4,83 @@ DATASET_PATH = 'C:\\Users\\Prithwish Dan\\Documents\\SP23 Classes\\CS4300\\archi
 RESTAURANT_PATH = DATASET_PATH + '\\restaurant_table.csv'
 RESTAURANT_MENUS_PATH = DATASET_PATH + '\\item_table.csv'
 
-# orig = pd.read_csv(DATASET_PATH + '\\restaurants.csv')
+orig = pd.read_csv(DATASET_PATH + '\\restaurants.csv')
 # data = pd.read_csv(RESTAURANT_PATH)
-df = pd.read_csv(RESTAURANT_MENUS_PATH)
-pattern = re.compile('[^A-z0-9 ]+')
+# df = pd.read_csv(RESTAURANT_MENUS_PATH)
+# pattern = re.compile('[^A-z0-9 ]+')
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+abb_to_us_state = {v:k for k,v in us_state_to_abbrev.items()}
 
-def clean_text(string):
-  if type(string) != str:
-    return string
-  return pattern.search(string)
+def get_state(full_addr):
+    if(type(full_addr) != str):
+        return 'None'
+    terms = [t.strip() for t in full_addr.split(',')]
+    for term in terms:
+        if len(term) == 2 and term == term.upper() and term.isalpha():
+            return abb_to_us_state[term]
+    return 'n/a'
+# def clean_text(string):
+#   if type(string) != str:
+#     return string
+#   return pattern.search(string)
 def fix_ampersand(text):
   if(type(text) != str):
       return 'None'
@@ -26,14 +94,15 @@ def fix_ampersand(text):
 # orig = orig.sort_values(by=['id'])
 # print(ids)
 # print(orig['id'])
-# data['name'] = orig['name'].apply(fix_ampersand)
-# data['category'] = data['category'].apply(fix_ampersand)
+orig['name'] = orig['name'].apply(fix_ampersand)
+orig['category'] = orig['category'].apply(fix_ampersand)
+orig['state'] = orig['full_address'].apply(get_state)
 
-df['name'] = df['name'].apply(fix_ampersand)
-df['description'] = df['description'].apply(fix_ampersand)
-df['category'] = df['category'].apply(fix_ampersand)
-print(df.loc[75075, :])
+# df['name'] = df['name'].apply(fix_ampersand)
+# df['description'] = df['description'].apply(fix_ampersand)
+# df['category'] = df['category'].apply(fix_ampersand)
+# print(df.loc[75075, :])
 # print(data['name'])
 
-# data.to_csv(DATASET_PATH + '\\restaurant_table.csv')
-df.to_csv(DATASET_PATH + '\\item_table.csv')
+orig.to_csv(DATASET_PATH + '\\restaurant_table.csv')
+# df.to_csv(DATASET_PATH + '\\item_table.csv')
