fc251399670d3d113267316a4f4f0238be505d4b
Prithwish Dan <dan.prithwish@gmail.com>
2023-04-16 10:00:57 -0400

    This class is trolling me

--- init.sql
+++ init.sql
@@ -31,7 +31,7 @@ CREATE TABLE `item_table` (
   `name` text,
   `description` text,
   `price` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +64,7 @@ CREATE TABLE `restaurant_table` (
   `lat` double DEFAULT NULL,
   `lng` double DEFAULT NULL,
   `state` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
