ad3429840527f1385700570b63ebdf1e7caceae1
Prithwish Dan <dan.prithwish@gmail.com>
2023-04-28 10:54:29 -0400

    Added placeholders, fixed no items found, improved price range info, added item description

---
+++ bruh.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS kardashiandb;
+USE kardashiandb;
+DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `restaurant`;
+DROP TABLE IF EXISTS `item_table`;
+DROP TABLE IF EXISTS `restaurant_table`;
